c5827d9c5de1e4f6f23d3199415983e0f5e52c76
sadeghi85 <sadeghi85@hotmail.com>
2014-01-07 07:21:22 -0500

    .

--- app/libraries/sadeghi85/domains.php
+++ app/libraries/sadeghi85/domains.php
@@ -6,7 +6,8 @@
 class Domains {
 
 	private static $utilitiesPath = '/opt/webpanel/cmd/utilities';
-	private static $utilitiesSignature = '*webpanel*';
+	private static $utilitiesBeginSignature = '*webpanel*';
+	private static $utilitiesEndSignature = '@webpanel@';
 	
 	private static $basePath = '/var/www/WebPanel';
 	private static $webDir = 'web';
@@ -29,105 +30,201 @@ class Domains {
 	{
 		self::initialize();
 		
-		$domain = escapeshellarg(strtolower($domain));
+		$domain = escapeshellcmd(strtolower($domain));
 		
 		// Check if domain directory already exists
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s/%s', self::$utilitiesPath, 'dir_exists.sh', self::$basePath, $domain), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s/%s"', self::$utilitiesPath, 'file_exists.sh', self::$basePath, $domain), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "dir_exists.sh" utility.');
+			if ( ! preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "file_exists.sh" utility.');
+			}
 		}
 		
 		if ($returnVal === 0)
 		{
-			return array('status' => '1', 'message' => 'Domain directory already exists. Move it and try again.');
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Domain directory already exists. Move it and try again.');
 		}
+		
+		unset($output);
 		//
 		
 		// Create the domain directory
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s/%s', self::$utilitiesPath, 'mkdir.sh', self::$basePath, $domain), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s/%s"', self::$utilitiesPath, 'mkdir.sh', self::$basePath, $domain), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "mkdir.sh" utility.');
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t create the Domain directory.');
+			}
+		
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "mkdir.sh" utility.');
 		}
 		
-		if ($returnVal === 1)
-		{
-			return array('status' => '1', 'message' => 'Couldn\'t create the Domain directory.');
-		}
+		unset($output);
 		//
 		
 		// Create the web directory
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s/%s/%s', self::$utilitiesPath, 'mkdir.sh', self::$basePath, $domain, $webDir), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s/%s/%s"', self::$utilitiesPath, 'mkdir.sh', self::$basePath, $domain, self::$webDir), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "mkdir.sh" utility.');
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t create the web directory.');
+			}
+			
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "mkdir.sh" utility.');
 		}
 		
-		if ($returnVal === 1)
-		{
-			return array('status' => '1', 'message' => 'Couldn\'t create the web directory.');
-		}
+		unset($output);
 		//
 		
 		// PHP user
 		$phpUser = 'u-'.substr(str_replace('.', '_', $domain), 0, 30);
 		
 		// Check if PHP user already exists
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s', self::$utilitiesPath, 'user_exists.sh', $phpUser), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s"', self::$utilitiesPath, 'user_exists.sh', $phpUser), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "user_exists.sh" utility.');
+			// PHP user for this domain doesn't exists
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				unset($output);
+				
+				// Create the PHP user
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s" "%s/%s" "%s"', self::$utilitiesPath, 'useradd.sh', $phpUser, self::$basePath, $domain, self::$userShell), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t create the PHP user for this domain.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "useradd.sh" utility.');
+				}
+				
+				unset($output);
+				//
+			}
+			else
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "user_exists.sh" utility.');
+			}
 		}
 		
-		// PHP user for this domain doesn't exists
-		if ($returnVal === 1)
-		{
-			// Create the PHP user
-			if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s %s/%s %s', self::$utilitiesPath, 'useradd.sh', $phpUser, self::$basePath, $domain, self::$userShell), $output, $returnVal))
-			{
-				return array('status' => '1', 'message' => 'Critical error in executing "useradd.sh" utility.');
-			}
-			
-			if ($returnVal === 1)
-			{
-				return array('status' => '1', 'message' => 'Couldn\'t create the PHP user for this domain.');
-			}
-			//
-		}
+		unset($output);
 		//
 		
 		// Correcting permissions on the domain directory
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s %s %s/%s', self::$utilitiesPath, 'chown.sh', '-R', $phpUser, self::$basePath, $domain), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s" "%s" "%s/%s"', self::$utilitiesPath, 'chown.sh', '-R', $phpUser, self::$basePath, $domain), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "chown.sh" utility.');
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t change permissions on the domain directory.');
+			}
+			
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "chown.sh" utility.');
 		}
 		
-		if ($returnVal === 1)
-		{
-			return array('status' => '1', 'message' => 'Couldn\'t change permissions on the domain directory.');
-		}
+		unset($output);
 		
 		// 644 for files (initially 644 for all, then 755 for dirs)
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s %s/%s', self::$utilitiesPath, 'chmod.sh', '-R 644', self::$basePath, $domain), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s" "%s/%s"', self::$utilitiesPath, 'chmod.sh', '-R 644', self::$basePath, $domain), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "chmod.sh" utility.');
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t change permissions on the domain directory.');
+			}
+			
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "chmod.sh" utility.');
 		}
 		
-		if ($returnVal === 1)
-		{
-			return array('status' => '1', 'message' => 'Couldn\'t change permissions on the domain directory.');
-		}
+		unset($output);
 		
 		// to give search bit to all directories, effectively 755 for dirs
-		if (self::$utilitiesSignature != exec(sprintf('sudo sh %s/%s %s %s/%s', self::$utilitiesPath, 'chmod.sh', '-R +X', self::$basePath, $domain), $output, $returnVal))
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "%s" "%s/%s"', self::$utilitiesPath, 'chmod.sh', '-R +X', self::$basePath, $domain), $output, $returnVal))
 		{
-			return array('status' => '1', 'message' => 'Critical error in executing "chmod.sh" utility.');
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t change permissions on the domain directory.');
+			}
+			
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "chmod.sh" utility.');
 		}
 		
-		if ($returnVal === 1)
-		{
-			return array('status' => '1', 'message' => 'Couldn\'t change permissions on the domain directory.');
-		}
+		unset($output);
 		//
 		
-		
+		// Creating pool definition
+		if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "/etc/php-fpm.d/sites-available/%s.conf"', self::$utilitiesPath, 'file_exists.sh', $domain), $output, $returnVal))
+		{
+			// Pool definition doesn't exists
+			if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+			{
+				unset($output);
+				
+				// Copy pool definition from template
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "templates/php-fpm/example.com.conf" "/etc/php-fpm.d/sites-available/%s.conf"', self::$utilitiesPath, 'cp.sh', $domain), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t copy the pool definition from template.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "cp.sh" utility.');
+				}
+				
+				unset($output);
+				
+				// Regex replace
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "s/example\\\.com/%s/g" "/etc/php-fpm.d/sites-available/%s.conf"', self::$utilitiesPath, 'regex_replace.sh', $domain, $domain), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t edit the pool definition.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "regex_replace.sh" utility.');
+				}
+				
+				unset($output);
+				
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "s/^user\\\s\\\+=.*/user = %s/" "/etc/php-fpm.d/sites-available/%s.conf"', self::$utilitiesPath, 'regex_replace.sh', $phpUser, $domain), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t edit the pool definition.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "regex_replace.sh" utility.');
+				}
+				
+				unset($output);
+				
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "s/^group\\\s\\\+=.*/group = %s/" "/etc/php-fpm.d/sites-available/%s.conf"', self::$utilitiesPath, 'regex_replace.sh', $phpUser, $domain), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t edit the pool definition.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "regex_replace.sh" utility.');
+				}
+				
+				unset($output);
+				
+				if (self::$utilitiesBeginSignature != exec(sprintf('sudo sh "%s/%s" "../sites-available/%s.conf" "/etc/php-fpm.d/sites-enabled/%s.conf"', self::$utilitiesPath, 'ln.sh', $domain, $domain), $output, $returnVal))
+				{
+					if (preg_match(sprintf('#%s#', self::$utilitiesEndSignature), implode("\n", $output)))
+					{
+						return array('status' => '1', 'message' => 'Line '.__LINE__.': Couldn\'t enable the pool.');
+					}
+					
+					return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "ln.sh" utility.');
+				}
+				
+				unset($output);
+			}
+			
+			return array('status' => '1', 'message' => 'Line '.__LINE__.': Critical error in executing "file_exists.sh" utility.');
+		}
+		//
 		
 		
 		
